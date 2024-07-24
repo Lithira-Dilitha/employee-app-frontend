b79785472222ce7bba407882416e35a26b67f066
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-24 17:40:18 +0530

    implement addEmployee Function

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
-
+  public employeeobj = {
+    firstName:"",
+    lastName:"",
+    email:"",
+    departmentId:"",
+    roleId:""
+  }
+  addEmployee(){
+    console.log(this.employeeobj);
+  }
 }
