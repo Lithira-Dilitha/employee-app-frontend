8fddd27f698bc547b3d96b25930c79bff1a04940
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-25 11:58:43 +0530

    implemented navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
 import { ViewAllEmployeesComponent } from './page/view-all-employees/view-all-employees.component';
+import { NavComponent } from "./common/nav/nav.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmpComponent,ViewAllEmployeesComponent],
+  imports: [RouterOutlet, ManageEmpComponent, ViewAllEmployeesComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
